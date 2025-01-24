5c560d1d25c1391cea52d92d9bcc3a1e554688fa
Hunter <hunterh.online@gmail.com>
2025-01-24 11:55:03 -0500

    fixes

---
+++ Mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobile
+//
+//  Created by Hunter Harris on 1/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mobile/MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by Hunter Harris on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
